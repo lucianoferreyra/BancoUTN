26f5cc934050131f8f501b26557f9cd62ed35ad8
lucianoferreyra <luchooferreyra@gmail.com>
2022-09-30 17:17:32 -0300

    navegacion entre actividades

--- app/src/main/java/com/ferreyra/molet/paduli/MainActivity.java
+++ app/src/main/java/com/ferreyra/molet/paduli/MainActivity.java
@@ -2,13 +2,29 @@ package com.ferreyra.molet.paduli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button simular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simular = (Button)findViewById(R.id.botonSimular);
+
+        simular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SimularPlazoFijo.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/ferreyra/molet/paduli/SimularPlazoFijo.java
@@ -0,0 +1,14 @@
+package com.ferreyra.molet.paduli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimularPlazoFijo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simular_plazo_fijo);
+    }
+}
