70b7a5ecb605cb71143e04f40ce17b47543508b3
padu818 <paduli.18@gmail.com>
2022-10-05 23:53:54 -0300

    metodo calcular

--- app/src/main/java/com/ferreyra/molet/paduli/SimularPlazoFijo.java
+++ app/src/main/java/com/ferreyra/molet/paduli/SimularPlazoFijo.java
@@ -14,11 +14,16 @@ import android.widget.TextView;
 
 public class SimularPlazoFijo extends AppCompatActivity {
     Button confirmarButton;
-    TextView plazoText,capitalText,interesesText,montoTotalText,montoTotalAnualText;
+    TextView plazoText;
+    TextView capitalText;
+    TextView interesesText;
+
+    TextView montoTotalText;
+    TextView montoTotalAnualText;
     private EditText capital, tasaNominal, tasaEfectiva;
     private TextView textDias;
     private SeekBar seekBarPlazo;
-
+    int mes = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +46,9 @@ public class SimularPlazoFijo extends AppCompatActivity {
         seekBarPlazo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textDias.setText(String.valueOf(i*30)+ " dias");
-                plazoText.setText(textDias.getText());
+                mes = i;
+                calcular(i);
+
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -51,6 +57,9 @@ public class SimularPlazoFijo extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+
+
         tasaNominal.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -60,18 +69,22 @@ public class SimularPlazoFijo extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable editable) {
+
             }
         });
         tasaEfectiva.addTextChangedListener(new TextWatcher() {
+
+
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
             @Override
             public void afterTextChanged(Editable editable) {
-
+                calcular(mes);
             }
         });
         capital.addTextChangedListener(new TextWatcher() {
@@ -80,18 +93,53 @@ public class SimularPlazoFijo extends AppCompatActivity {
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                calcular(mes);
             }
             @Override
             public void afterTextChanged(Editable editable) {
-                capitalText.setText(capital.getText());
+
             }
         });
     }
 
+    private void calcular(int dias) {
+        double tasaEfectivaValue = 0;
+        double tasaEfectivaMensualValue = 0;
+        double interesGanado = 0;
+        double capitalValue = 0;
+        double interesGanadoAnual = 0;
+        double montoTotal = 0;
+        double montoTotalAnual = 0;
+        double tasaNominalValue = 0;
+        double tasaNominalMensualValue = 0;
+        textDias.setText(String.valueOf(dias*30)+ " dias");
+        plazoText.setText(textDias.getText());
+        if(capital.getText().toString().length() > 0 && tasaEfectiva.getText().toString().length() > 0
+                && tasaNominal.getText().toString().length() > 0) {
+            capitalText.setText(capital.getText());
+            tasaEfectivaValue = Double.valueOf(tasaEfectiva.getText().toString());
+            tasaNominalValue =Double.valueOf(tasaNominal.getText().toString());
+            tasaNominalMensualValue = (tasaNominalValue/100) / 12;
+            tasaEfectivaMensualValue = (tasaEfectivaValue/100) / 12;
+            capitalValue = Double.valueOf(capital.getText().toString());
+            interesGanado = capitalValue * (tasaNominalMensualValue)*dias;
+            interesGanadoAnual = capitalValue* (tasaNominalMensualValue)*12;
+            montoTotal = capitalValue + interesGanado;
+            montoTotalAnual = capitalValue + interesGanadoAnual;
+
+
+
+            interesesText.setText(""+interesGanado);
+            montoTotalText.setText(""+montoTotal);
+            montoTotalAnualText.setText(""+montoTotalAnual);
+        }
+    }
 
 
     public void onClick(View v){
-       // plazoText.setText(R.id.);
+        if(capital.getText().toString().length() > 0 && tasaEfectiva.getText().toString().length() > 0
+                && tasaNominal.getText().toString().length() > 0) {
 
+        }
     }
 }
